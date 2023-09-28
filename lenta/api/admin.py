bee1a5fd5a0b14e3630495e9661521bd72f7dfde
Вячеслав Мельник <jurcenter@yandex.ru>
2023-09-28 23:27:57 +0300

    Построение модели forecast

--- lenta/api/admin.py
+++ lenta/api/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from categories.models import Categories
+from forecast.models import Forecast, SalesUnits
+from sales.models import Sales
+from shops.models import Shops
 
-# Register your models here.
+
+class CategoriesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Categories)
+
+
+class ForecastAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Forecast)
+
+
+class SalesUnitsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(SalesUnits)
+
+
+class SalesAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Sales)
+
+
+class ShopsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Shops)
