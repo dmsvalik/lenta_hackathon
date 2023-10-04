d33a308b9db3d5f7b166d830de0a4e4c2175ab0f
Никита <0vodkacoffee@gmail.com>
2023-10-04 20:00:37 +0300

    resolved merge problems

--- lenta/api/urls.py
+++ lenta/api/urls.py
@@ -2,13 +2,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from api.views import (
-    CategoriesViewSet,
+    CategoriesAPIView,
     ForecastCreateAPIView,
     ForecastViewSet,
     SalesViewSet,
-    ShopsViewSet
+    ShopsAPIView
 )
-from users.views import CustomAuthToken
 
 app_name = 'api'
 
@@ -19,7 +18,7 @@ urlpatterns = [
     path('', include(router_v1.urls)),
     path('forecast/', ForecastViewSet.as_view({'get': 'list'}), name='forecast'),
     path('forecast/create/', ForecastCreateAPIView.as_view(), name='forecast-create'),
-    path('categories/', CategoriesViewSet.as_view({'get': 'list'}), name='categories'),
+    path('categories/', CategoriesAPIView.as_view({'get': 'list'}), name='categories'),
     path('sales/', SalesViewSet.as_view({'get': 'list'}), name='sales'),
-    path('shops/', ShopsViewSet.as_view({'get': 'list'}), name='shops'),
+    path('shops/', ShopsAPIView.as_view(), name='shops'),
 ]
