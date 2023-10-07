a6ae25c84338e0b77f7b6d4de757176ecd0d7098
Никита <0vodkacoffee@gmail.com>
2023-10-07 21:02:00 +0300

    added users model and auth system

--- lenta/lenta/settings.py
+++ lenta/lenta/settings.py
@@ -11,9 +11,14 @@ ALLOWED_HOSTS = []
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
 }
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 # Application definition
 
 INSTALLED_APPS = [
--- lenta/users/models.py
+++ lenta/users/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=100, blank=True)
+    name = models.CharField(max_length=50, blank=True)
+    surname = models.CharField(max_length=50, blank=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ('password',)
 
